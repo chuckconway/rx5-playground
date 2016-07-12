62b73347150ad62c99bbdca204e0e52b6ae27e02
Chuck Conway <charles.conway@libertyhomeequity.com>
2016-07-12 14:59:03 -0700

    Functions

---
+++ src-server/example_03_from.js
@@ -0,0 +1,34 @@
+import Rx from 'rxjs/Rx';
+import {createSubscriber} from './lib/util';
+import fs from 'fs';
+
+//old
+// fs.readdir('./src-server', (err, items) = > {
+//
+//   if(err){
+//     console.error(err);
+//   } else {
+//     console.log(items);
+//   }
+// });
+
+
+//From, mergeMap, Map
+// const readdir$ = Rx.Observable.bindNodeCallback(fs.readdir);
+//
+// readdir$('./src-server')
+//   .mergeMap(files => Rx.Observable.from(files))
+//   .map(file => `Manipulated ${file}`)
+//   .subscribe(createSubscriber('readdir'));
+
+//Promise
+function getItem(){
+  return new Promise((resolve, reject) =>{
+     setTimeout(()=> {
+       resolve("Hello");
+     }, 1000);
+  });
+}
+
+Rx.Observable.fromPromise(getItem())
+  .subscribe(createSubscriber('promise'));
