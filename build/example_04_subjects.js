cb7f935fef37b5681d0656b31355b543d7fe58bf
chuckconway <charles@cconway.com>
2016-07-15 17:22:19 -0700

    Subjects

--- build/example_04_subjects.js
+++ build/example_04_subjects.js
@@ -12,23 +12,70 @@ var _fs2 = _interopRequireDefault(_fs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var simple$ = new _Rx2.default.Subject();
+// const simple$ = new Rx.Subject();
+//
+// simple$.subscribe(createSubscriber('simple$'));
+//
+// simple$.next('hello');
+// simple$.next('world');
+// simple$.complete();
+//
+//
+// const interval$ = Rx.Observable.interval(1000).take(5);
+// const intervalSubject$ = new Rx.Subject();
+//
+// interval$.subscribe(intervalSubject$)
+//
+// intervalSubject$.subscribe(createSubscriber('sub1'));
+// intervalSubject$.subscribe(createSubscriber('sub2'));
+// intervalSubject$.subscribe(createSubscriber('sub3'));
+//
+//
+// setTimeout(()=> {
+//     intervalSubject$.subscribe(createSubscriber('look at me!'));
+// }, 3000)
 
-simple$.subscribe((0, _util.createSubscriber)('simple$'));
+// const currentUser$ = new Rx.Subject();
+// const isloggedIn$  = currentUser$.map(u => u.isloggedIn);
+//
+// isLoggedIn$.subscribe(createSubscriber("isLoggedIn"));
+//
+// currentUser$.next({isLoggedIn:false});
+//
+// setTimeout(() => {
+//     currentUser$.next({isLoggedIn:true, name:'chuck'});
+// }, 2000);
 
-simple$.next('hello');
-simple$.next('world');
-simple$.complete();
+// const currentUser$ = new Rx.BehaviorSubject({isloggedIn:false});
+// const isloggedIn$  = currentUser$.map(u => u.isloggedIn);
+//
+// isLoggedIn$.subscribe(createSubscriber("isLoggedIn"));
+//
+// currentUser$.next({isLoggedIn:false});
+//
+// setTimeout(() => {
+//     currentUser$.next({isLoggedIn:true, name:'chuck'});
+// }, 2000);
 
-var interval$ = _Rx2.default.Observable.interval(1000).take(5);
-var intervalSubject$ = new _Rx2.default.Subject();
+// const replay$ = new Rx.ReplaySubject(3);
+// replay$.next(1);
+// replay$.next(2);
+//
+// replay$.subscribe(createSubscriber("one"));
+//
+// replay$.next(3);
+// replay$.next(4);
+// replay$.next(5);
+//
+// replay$.subscribe(createSubscriber("two"));
 
-interval$.subscribe(intervalSubject$);
+var apiCall$ = new _Rx2.default.AsyncSubject();
+apiCall$.next(1);
 
-intervalSubject$.subscribe((0, _util.createSubscriber)('sub1'));
-intervalSubject$.subscribe((0, _util.createSubscriber)('sub2'));
-intervalSubject$.subscribe((0, _util.createSubscriber)('sub3'));
+apiCall$.subscribe((0, _util.createSubscriber)("one"));
+
+apiCall$.next(2);
 
 setTimeout(function () {
-    intervalSubject$.subscribe((0, _util.createSubscriber)('look at me!'));
-}, 3000);
+    apiCall$.subscribe((0, _util.createSubscriber)("one"));
+}, 2000);
--- src-server/example_04_subjects.js
+++ src-server/example_04_subjects.js
@@ -2,25 +2,72 @@ import Rx from 'rxjs/Rx';
 import {createSubscriber} from './lib/util';
 import fs from 'fs';
 
-const simple$ = new Rx.Subject();
+// const simple$ = new Rx.Subject();
+//
+// simple$.subscribe(createSubscriber('simple$'));
+//
+// simple$.next('hello');
+// simple$.next('world');
+// simple$.complete();
+//
+//
+// const interval$ = Rx.Observable.interval(1000).take(5);
+// const intervalSubject$ = new Rx.Subject();
+//
+// interval$.subscribe(intervalSubject$)
+//
+// intervalSubject$.subscribe(createSubscriber('sub1'));
+// intervalSubject$.subscribe(createSubscriber('sub2'));
+// intervalSubject$.subscribe(createSubscriber('sub3'));
+//
+//
+// setTimeout(()=> {
+//     intervalSubject$.subscribe(createSubscriber('look at me!'));
+// }, 3000)
 
-simple$.subscribe(createSubscriber('simple$'));
-
-simple$.next('hello');
-simple$.next('world');
-simple$.complete();
-
-
-const interval$ = Rx.Observable.interval(1000).take(5);
-const intervalSubject$ = new Rx.Subject();
-
-interval$.subscribe(intervalSubject$)
-
-intervalSubject$.subscribe(createSubscriber('sub1'));
-intervalSubject$.subscribe(createSubscriber('sub2'));
-intervalSubject$.subscribe(createSubscriber('sub3'));
+// const currentUser$ = new Rx.Subject();
+// const isloggedIn$  = currentUser$.map(u => u.isloggedIn);
+//
+// isLoggedIn$.subscribe(createSubscriber("isLoggedIn"));
+//
+// currentUser$.next({isLoggedIn:false});
+//
+// setTimeout(() => {
+//     currentUser$.next({isLoggedIn:true, name:'chuck'});
+// }, 2000);
 
 
-setTimeout(()=> {
-    intervalSubject$.subscribe(createSubscriber('look at me!'));
-}, 3000)
+// const currentUser$ = new Rx.BehaviorSubject({isloggedIn:false});
+// const isloggedIn$  = currentUser$.map(u => u.isloggedIn);
+//
+// isLoggedIn$.subscribe(createSubscriber("isLoggedIn"));
+//
+// currentUser$.next({isLoggedIn:false});
+//
+// setTimeout(() => {
+//     currentUser$.next({isLoggedIn:true, name:'chuck'});
+// }, 2000);
+
+
+// const replay$ = new Rx.ReplaySubject(3);
+// replay$.next(1);
+// replay$.next(2);
+//
+// replay$.subscribe(createSubscriber("one"));
+//
+// replay$.next(3);
+// replay$.next(4);
+// replay$.next(5);
+//
+// replay$.subscribe(createSubscriber("two"));
+
+const apiCall$ = new Rx.AsyncSubject();
+apiCall$.next(1);
+
+apiCall$.subscribe(createSubscriber("one"));
+
+apiCall$.next(2);
+
+setTimeout(() => {
+    apiCall$.subscribe(createSubscriber("one"));
+}, 2000);
